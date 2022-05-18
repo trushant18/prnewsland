2187aa4e5528c41f027361339ffb5d19d9005280
Kishan Ambaliyala <kishan.urvam@gmail.com>
2022-05-18 22:51:14 +0530

    Corrected meta information related changes

--- app/Http/Controllers/Front/BlogController.php
+++ app/Http/Controllers/Front/BlogController.php
@@ -20,7 +20,9 @@ class BlogController extends Controller
         if (is_null($blog)){
             return redirect()->route('home');
         }
-        return view('front.blogs.single', compact('blog'));
+        $meta['meta_title'] = $blog->title;
+        $meta['meta_description'] = $blog->short_content;
+        return view('front.blogs.single', compact('blog', 'meta'));
     }
 
     public function getNewsList()
--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -4,11 +4,11 @@
 <!-- Mirrored from miles.tortoizthemes.com/live-final/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Jul 2021 16:01:45 GMT -->
 <head>
     <meta charset="UTF-8">
-    <title>PRNewsland</title>
+    <title>{{ $meta['meta_title'] ?? "PRNewsland" }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="PRNEWSLAND Website"/>
+    <meta name="description" content="{{ $meta['meta_description'] ?? "PRNEWSLAND Website" }}"/>
     <meta name="author" content="shail">
-    <meta name="keywords" content=""/>
+    <meta name="keywords" content="{{ $meta['meta_keyword'] ?? "" }}"/>
     <link rel="icon" href="{{ url('front/images/favicon.png') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('front/assets/css/main.min.css') }}">
     @yield('css')
