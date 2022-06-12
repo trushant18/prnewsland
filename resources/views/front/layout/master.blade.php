ba876f68205b625ad401a8bb5e450347030646ed
Kishan Ambaliyala <kishan.urvam@gmail.com>
2022-06-12 10:10:08 +0530

    Corrected meta information

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -4,9 +4,9 @@
 <!-- Mirrored from miles.tortoizthemes.com/live-final/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Jul 2021 16:01:45 GMT -->
 <head>
     <meta charset="UTF-8">
-    <title>{{ $meta['meta_title'] ?? "PRNewsland" }}</title>
+    <title>{{ $meta['meta_title'] ?? "Free Press Release Submission with PRNews Land" }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="{{ $meta['meta_description'] ?? "PRNEWSLAND Website" }}"/>
+    <meta name="description" content="{{ $meta['meta_description'] ?? "PRNews Land Helps you to reach and engage with target audiences across the globe with notable increased online visibility." }}"/>
     <meta name="author" content="shail">
     <meta name="keywords" content="{{ $meta['meta_keyword'] ?? "" }}"/>
     <link rel="icon" href="{{ url('front/images/favicon.png') }}">
